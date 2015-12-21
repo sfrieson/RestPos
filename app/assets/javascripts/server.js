0436c394a40095880c6897e24445d57d808f0e80
Steven Frieson <sfrieson@gmail.com>
2015-12-21 15:03:27 -0500

    Adds close button to receipt modal

--- app/assets/javascripts/server.js
+++ app/assets/javascripts/server.js
@@ -1,14 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+var closeBtn = '<i class="fa fa-times-circle close-btn fa-3x"></i>'
+var modalBg = $('<div>').addClass('modal-bg')
+
 var receipts = $('.receipt')
 receipts.on('click', function(e) {
     e.preventDefault()
-    var modal_bg = $('<div>').addClass('modal-bg')
-    modal_bg.on('click', function(){modal_bg.remove()})
-    var a = $(e.currentTarget)
-    var path = a.attr('href')
-    var iframe = $('<iframe>').attr('src', path).addClass('modal')
-    modal_bg.append(iframe);
-    $('body').append(modal_bg);
+
+    var modal = $('<div>')
+
+    modalBg.append(closeBtn);
+    modalBg.on('click', function(){modal.remove()});
+    $(closeBtn).on('click', function(){modal.remove()});
+    var a = $(e.currentTarget);
+    var path = a.attr('href');
+    var iframe = $('<iframe>').attr('src', path).addClass('modal');
+    modalBg.append(iframe);
+    modal.append(modalBg);
+    $('body').append(modal);
 });
